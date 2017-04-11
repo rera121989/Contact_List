bb4786de4e3348c593ebd001a1e717c897a31e34
Raymond Romero <rera121989@yahoo.com>
2017-04-11 10:52:41 -0700

    Add controllers and attemp to add favicon

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,11 +4,8 @@ myApp.controller('AppCtrl', ['$scope', '$http',
 	function($scope, $http){
 		console.log("message from controller");
 
-		person1 = {name: "Mark", email: "Mart@marc.com", number: "(323) 823-8347"};
-		person2 = {name: "Chuck", email: "chuck@chuck.com", number: "(323) 947-1453"};
-		person3 = {name: "Lilo", email: "lilo@lilo.com", number: "(323) 743-0983"};
-
-		contacts = [person1, person2, person3];
-		$scope.contactList = contacts;
-
+	$http.get('/contactList').then(function(response){
+		console.log("Got data!");
+			$scope.contactList = response.data;
+	});
 }]);
--- server.js
+++ server.js
@@ -12,6 +12,19 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList', function(req,res){
+	//returns this to the server as users refresh this page or request
+	console.log("I received a GET request, sending data");
+
+
+		person1 = {name: "Mark", email: "Mart@marc.com", number: "(323) 823-8347"};
+		person2 = {name: "Chuck", email: "chuck@chuck.com", number: "(323) 947-1453"};
+		person3 = {name: "Lilo", email: "lilo@lilo.com", number: "(323) 743-0983"};
+
+		contacts = [person1, person2, person3];
+		res.json(contacts);
+});
+
 
 app.listen(3000);
 console.log("Server Running on port 3000");
