383faff9d1aae68636b98a3593ba9b5a1f7e0a78
Raymond Romero <rera121989@yahoo.com>
2017-04-18 09:16:50 -0700

    Add change to script

--- mongo_script.js
+++ mongo_script.js
@@ -1,9 +1,22 @@
-var db = connect('/name');
+var Pokemon = connect('/name');
 
 print('* Database Created');
 
-db.names.insert({'name': 'Don Draper'});
+Pokemon.names.insert(	{'name': 'Pikachu', 'hitpoints': '20pts'}, 
+						{'name': 'Squirtle', 'hitpoints': '22pts'},
+						{'name': 'Charmander', 'hitpoints': '25pts'}, 
+						{'name': 'Bulbasaur', 'hitpoints': '23pts'},
+						);
 
-allMadMen = db.names.find();
+allPokemon = Pokemon.names.find();
 
-print('* Documents created');
+Pokemon.names.remove({name: 'Bulbasaur'},{name: 'Squirtle'});
+
+Pokemon.names.update({hitpoints: '20'},{hitpoints: '22'});
+Pokemon.names.update({hitpoints: '25'},{hitpoints: '26'});
+allPokemon = Pokemon.names.find();
+
+Pokemon.names.find({name: 'Pikachu'});
+
+
+print('* Pokemon Created');
